b1a195eb813a53adcee3d65ac432687b6250fcce
VinhGiang731 <dinhvinhgiang345@gmail.com>
2024-11-15 00:52:09 +0700

    add SQLite

--- app/src/main/java/com/example/to_do_list/controller/Note_fragment.kt
+++ app/src/main/java/com/example/to_do_list/controller/Note_fragment.kt
@@ -1,34 +1,40 @@
 package com.example.to_do_list.controller
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.to_do_list.R
+import com.example.to_do_list.data.MyHelper
 import com.example.to_do_list.databinding.FragmentNoteViewBinding
 import com.example.to_do_list.model.ListNote
 import com.example.to_do_list.model.Rv_NoteAdapter
 
 class Note_fragment : Fragment(R.layout.fragment_note_view) {
-   private lateinit var binding: FragmentNoteViewBinding
-   private lateinit var adapter: Rv_NoteAdapter
+    private lateinit var binding: FragmentNoteViewBinding
+    private lateinit var adapter: Rv_NoteAdapter
+    private lateinit var db: SQLiteDatabase
+    private lateinit var rs: Cursor
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentNoteViewBinding.bind(view)
 
+        val helper = MyHelper(requireContext())
+        db = helper.readableDatabase
+        rs = db.rawQuery("SELECT *FROM TODOLIST", null)
+
         val list = mutableListOf<ListNote>()
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
-        list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
+        if (rs.moveToFirst()) {
+            do {
+                val note = rs.getString(rs.getColumnIndexOrThrow("NOTE"))
+                val date = rs.getString(rs.getColumnIndexOrThrow("DATETIME"))
+                list.add(ListNote(note, date))
+            } while (rs.moveToNext())
+        }
 
         adapter = Rv_NoteAdapter(requireActivity(), list)
 
--- app/src/main/java/com/example/to_do_list/data/MyHelper.kt
+++ app/src/main/java/com/example/to_do_list/data/MyHelper.kt
@@ -1,4 +1,23 @@
 package com.example.to_do_list.data
 
-class MyHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyHelper(context : Context) :SQLiteOpenHelper(context, "TODOLIST_DB", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE TODOLIST(DATETIME TEXT, NOTE TEXT)")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'First note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Second note with me')")
+        db?.execSQL("INSERT INTO TODOLIST(DATETIME, NOTE) VALUES('31/07/2004', 'Testtttttttttttttttt')")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
 }
