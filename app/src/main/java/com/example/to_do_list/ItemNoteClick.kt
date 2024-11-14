6ada7eac51545d29c90f235ac4b96fc4fb87e85d
VinhGiang731 <dinhvinhgiang345@gmail.com>
2024-11-14 23:57:13 +0700

    change list view to recycle view

---
+++ app/src/main/java/com/example/to_do_list/ItemNoteClick.kt
@@ -0,0 +1,5 @@
+package com.example.to_do_list
+
+interface ItemNoteClick {
+    fun onClickNote(pos: Int)
+}
--- app/src/main/java/com/example/to_do_list/Note_fragment.kt
+++ app/src/main/java/com/example/to_do_list/Note_fragment.kt
@@ -3,11 +3,12 @@ package com.example.to_do_list
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.to_do_list.databinding.FragmentNoteViewBinding
 
 class Note_fragment : Fragment(R.layout.fragment_note_view) {
    private lateinit var binding: FragmentNoteViewBinding
-   private lateinit var adapter: ListNoteAdapter
+   private lateinit var adapter: Rv_NoteAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,12 +27,10 @@ class Note_fragment : Fragment(R.layout.fragment_note_view) {
         list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
         list.add(ListNote("day chi la doan note tao lao ma tao viet khi deo co Evkey, va chi co the thoi. bien", "31/07/2004"))
 
-        adapter = ListNoteAdapter(requireActivity(), list)
+        adapter = Rv_NoteAdapter(requireActivity(), list)
 
-        binding.lvNote.adapter = adapter
+        binding.rvListNote.adapter = adapter
 
-        binding.lvNote.setOnItemClickListener { adapterView, view, i, l ->
-            binding.lvNote.setItemChecked(i, true)
-        }
+        binding.rvListNote.layoutManager = LinearLayoutManager(requireActivity())
     }
 }
